a932c9620cdff60dab3664f8b2c08965f3c48f86
Andrei <cad.dsw.dwa@gmail.com>
2022-08-28 18:00:48 +0500

    create from group

--- src/app/question-form/question-form.component.ts
+++ src/app/question-form/question-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
 import { Question } from 'src/Models/Question';
 import { QuestionService } from 'src/services/question.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question-form',
@@ -14,9 +14,17 @@ export class QuestionFormComponent implements OnInit, OnDestroy {
   unsubcribe: Subject<any> = new Subject<any>();
   isLoad: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   constructor(
-    private questionService: QuestionService
+    private questionService: QuestionService,
+    private formBuilder: FormBuilder,
   ) {}
-
+  questionForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    question1: [''],
+    question2: [''],
+    question3: [''],
+    question4: [''],
+    question5: [''],
+  });
   ngOnInit(): void {
     this.questionService.getAll()
     .pipe(
@@ -27,6 +35,11 @@ export class QuestionFormComponent implements OnInit, OnDestroy {
       this.isLoad.next(true);
       console.log(this.questionList, this.isLoad);
     })
+  }
+  onSubmit(){
+    if (this.questionForm.valid){
+      console.log(this.questionForm);
+    }
   }
   ngOnDestroy(): void {
     this.unsubcribe.next(undefined)
